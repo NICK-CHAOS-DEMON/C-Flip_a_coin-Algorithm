ef173ae4235ee1a6f5140c0ed31c48ad6bc16d42
NICK-CHAOS-DEMON <117721475+NICK-CHAOS-DEMON@users.noreply.github.com>
2022-12-01 09:19:04 -0800

    Add files via upload

---
+++ scratch.cpp
@@ -0,0 +1,25 @@
+//
+// Created by NickD on 11/5/2022.
+//
+#include <iostream>
+#include <string>
+#include <ostream>
+
+using namespace std;
+
+void flip_over(string x) {
+    int i = x.length();
+    int z = i;
+    string b;
+    int a;
+    for (a = 0; a < i; a += 1) {
+        char X = x[z -= 1];
+        b += X;}
+    cout << b << endl;
+}
+int main() {
+    string c;
+    c="123.567";
+    flip_over(c);
+    return 0;
+}
